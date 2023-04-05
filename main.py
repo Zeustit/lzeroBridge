b397496fdedce13c6046eaca53b0cf8be091094b
A.Godyaev <godyaev.andrey@gmail.com>
2023-04-05 15:42:33 +0300

    work started

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+from web3 import Web3
+from eth_account import Account
+from decimal import Decimal
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+address = ''
+RPC = [
+    {'chain': 'Ethereum     ', 'chain_id': 1, 'rpc': 'https://rpc.ankr.com/eth'},
+    {'chain': 'Goerli     ', 'chain_id': 5, 'rpc': 'https://goerli.blockpi.network/v1/rpc/public'},
+    {'chain': 'Optimism Goerli Testnet     ', 'chain_id': 420, 'rpc': 'https://optimism-goerli.public.blastapi.io'},
+    {'chain': 'Arbitrum One ', 'chain_id': 421613, 'rpc': 'https://goerli-rollup.arbitrum.io/rpc'},
+]
+ABI = '[{"inputs":[{"internalType":"address","name":"_weth","type":"address"},{"internalType":"address","name":"_oft","type":"address"},{"internalType":"address","name":"_swapRouter","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"oft","outputs":[{"internalType":"contract IOFTCore","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"poolFee","outputs":[{"internalType":"uint24","name":"","type":"uint24"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amountIn","type":"uint256"},{"internalType":"uint256","name":"amountOutMin","type":"uint256"},{"internalType":"uint16","name":"dstChainId","type":"uint16"},{"internalType":"address","name":"to","type":"address"},{"internalType":"address payable","name":"refundAddress","type":"address"},{"internalType":"address","name":"zroPaymentAddress","type":"address"},{"internalType":"bytes","name":"adapterParams","type":"bytes"}],"name":"swapAndBridge","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"swapRouter","outputs":[{"internalType":"contract ISwapRouter","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"weth","outputs":[{"internalType":"contract IWETH","name":"","type":"address"}],"stateMutability":"view","type":"function"}]'
+contract_address = '0x0A9f824C05A74F577A536A8A0c673183a872Dff4'
+# private key
+caller = '52e528d5f523ca7a97181e2eb3d67cc35f959dbe88e4442201d2565f31d09133'
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_balance(address, chain_number):
+    web3 = Web3(Web3.HTTPProvider(RPC[chain_number]['rpc']))
+    balance = web3.eth.get_balance(web3.to_checksum_address(address))
+
+    print(balance)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # print('Enter min eth amount to send:')
+    # min_amount = input()
+    #
+    # print('Enter max eth amount to send:')
+    # max_amount = input()
+
+    web3 = Web3(Web3.HTTPProvider('https://rpc.ankr.com/eth'))
+    contract_instance = web3.eth.contract(address=contract_address, abi=ABI)
+    check_balance('0x87d620A1aED74357b59fE2d05c41Bfa895caEcc8', 1)
+    check_balance('0x87d620A1aED74357b59fE2d05c41Bfa895caEcc8', 2)
+    check_balance('0x87d620A1aED74357b59fE2d05c41Bfa895caEcc8', 3)
+
+    # # initialize the chain id, we need it to build the transaction for replay protection
+    # Chain_id = web3.eth.chain_id
+
+    # Call your function
+    address = Account.from_key(caller).address
+    amountIn = web3.to_wei(Decimal('0.05'), 'ether')
+    amountOutMin = web3.to_wei(Decimal('0.04'), 'ether')
+    dstChainId = int('420', 16)
+    call_function = contract_instance.functions.swapAndBridge(amountIn, amountOutMin, dstChainId, address, address, address, 1).buildTransaction(
+        {"chainId": 5, "from": caller, "nonce": 1})
+    #
+    # Sign transaction
+    signed_tx = web3.eth.account.sign_transaction(call_function, private_key=caller)
+    #
+    # # Send transaction
+    # send_tx = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
+    #
+    # # Wait for transaction receipt
+    # tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
+    # # print(tx_receipt) # Optional
